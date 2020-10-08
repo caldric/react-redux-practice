6799954ac8c85a50edc05ad76fc679eb263cc3d8
caldric <cvaldric@gmail.com>
2020-10-07 20:33:10 -0400

    Convert Song Detail component to a function

--- songs/src/components/SongDetail.jsx
+++ songs/src/components/SongDetail.jsx
@@ -1,19 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 
-class SongDetail extends Component {
-  render() {
-    const { selectedSong } = this.props;
-    if (!selectedSong) return <div>Select a song</div>;
-    return (
-      <div>
-        <h2>Details For: </h2>
-        <p>Title: {selectedSong.title}</p>
-        <p>Artist: {selectedSong.artist}</p>
-      </div>
-    );
-  }
-}
+const SongDetail = ({ selectedSong }) => {
+  if (!selectedSong) return <div>Select a song</div>;
+  return (
+    <div>
+      <h2>Details For: </h2>
+      <p>Title: {selectedSong.title}</p>
+      <p>Artist: {selectedSong.artist}</p>
+    </div>
+  );
+};
 
 const mapStateToProps = (state) => {
   return { selectedSong: state.selectedSong };
