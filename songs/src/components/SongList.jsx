96dbe86a7ef7552493d5072b531f2be4328143b9
caldric <cvaldric@gmail.com>
2020-10-07 20:18:14 -0400

    Render songs from getSongs function

--- songs/src/components/SongList.jsx
+++ songs/src/components/SongList.jsx
@@ -1,9 +1,25 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { getSongs } from '../actions';
 
 class SongList extends Component {
+  componentDidMount() {
+    this.props.getSongs();
+  }
+
+  renderList() {
+    return this.props.songs.map((song) => {
+      return <div>{song.title}</div>;
+    });
+  }
+
   render() {
-    return <div>Song List</div>;
+    return <div>{this.renderList()}</div>;
   }
 }
 
-export default SongList;
+const mapStateToProps = (state) => {
+  return { songs: state.songs };
+};
+
+export default connect(mapStateToProps, { getSongs })(SongList);
