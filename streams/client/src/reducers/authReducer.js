6d2fcab1bc864ca2a6fb772411b4bd739bcb55e8
caldric <cvaldric@gmail.com>
2020-10-09 03:53:59 -0400

    Rewire GoogleAuth component to use Redux store

--- streams/client/src/reducers/authReducer.js
+++ streams/client/src/reducers/authReducer.js
@@ -1,7 +1,7 @@
 const INITIAL_STATE = { isSignedIn: null };
 
 export default (state = INITIAL_STATE, action) => {
-  switch (action) {
+  switch (action.type) {
     case 'SIGN_IN':
       return { ...state, isSignedIn: true };
     case 'SIGN_OUT':
