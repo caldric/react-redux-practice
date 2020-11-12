659c91297a6e3a60a006c8cc8941c4fb10ee569a
caldric <cvaldric@gmail.com>
2020-11-12 16:05:08 -0500

    Add labels to text inputs

--- streams/client/src/components/streams/StreamCreate.jsx
+++ streams/client/src/components/streams/StreamCreate.jsx
@@ -2,17 +2,26 @@ import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 
 class StreamCreate extends Component {
-  renderInput({ input }) {
+  renderInput({ input, label }) {
     // console.log(formProps);
     // return <input onChange={input.onChange} value={input.value} />;
-    return <input {...input} />;
+    return (
+      <div className="field">
+        <label>{label}</label>
+        <input {...input} />
+      </div>
+    );
   }
 
   render() {
     return (
-      <form>
-        <Field name="title" component={this.renderInput} />
-        <Field name="description" component={this.renderInput} />
+      <form className="ui form">
+        <Field name="title" component={this.renderInput} label="Enter Title" />
+        <Field
+          name="description"
+          component={this.renderInput}
+          label="Enter Description"
+        />
       </form>
     );
   }
