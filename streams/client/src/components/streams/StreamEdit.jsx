7def7b1859100c7fe885ce6fb91456ffdd68c445
caldric <cvaldric@gmail.com>
2020-12-01 18:56:40 -0500

    Display stream title on StreamEdit

--- streams/client/src/components/streams/StreamEdit.jsx
+++ streams/client/src/components/streams/StreamEdit.jsx
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const StreamEdit = ({ match }) => {
-  return <div>{`You are editing stream ID: ${match.params.id}`}</div>;
+import { connect } from 'react-redux';
+import { fetchStream } from '../../actions';
+
+class StreamEdit extends Component {
+  componentDidMount() {
+    const id = this.props.match.params.id;
+    this.props.fetchStream(id);
+  }
+
+  render() {
+    if (!this.props.stream) {
+      return <div>Loading...</div>;
+    }
+    return <div>{this.props.stream.title}</div>;
+  }
+}
+
+const mapStateToProps = (state, ownProps) => {
+  const id = ownProps.match.params.id;
+  return { stream: state.streams[id] };
 };
 
-export default StreamEdit;
+export default connect(mapStateToProps, { fetchStream })(StreamEdit);
