46556d02b7d216a096f840ff2d72c7dbb3d9cae3
caldric <cvaldric@gmail.com>
2020-12-10 14:48:41 -0500

    Build out StreamShow component

--- streams/client/src/components/streams/StreamShow.jsx
+++ streams/client/src/components/streams/StreamShow.jsx
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
-const StreamShow = () => {
-  return <div>StreamShow </div>;
+import { connect } from 'react-redux';
+import { fetchStream } from '../../actions';
+
+const StreamShow = ({ fetchStream, match, stream }) => {
+  const streamID = match.params.id;
+
+  useEffect(() => {
+    fetchStream(streamID);
+  }, [fetchStream, streamID]);
+
+  if (!stream) return <div>Loading...</div>;
+
+  return (
+    <div>
+      <h1>{stream.title}</h1>
+      <h5>{stream.description}</h5>
+    </div>
+  );
 };
 
-export default StreamShow;
+const mapStateToProps = (state, ownProps) => {
+  const id = ownProps.match.params.id;
+  return { stream: state.streams[id] };
+};
+
+export default connect(mapStateToProps, { fetchStream })(StreamShow);
