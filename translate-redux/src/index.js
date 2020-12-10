aae67430f855347322b7b7286e66169d05b827fd
caldric <cvaldric@gmail.com>
2020-12-10 18:51:57 -0500

    Enable Redux devtools functionality

--- translate-redux/src/index.js
+++ translate-redux/src/index.js
@@ -5,8 +5,13 @@ import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import reducers from './reducers';
 
+const store = createStore(
+  reducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
 ReactDOM.render(
-  <Provider store={createStore(reducers)}>
+  <Provider store={store}>
     <App />
   </Provider>,
   document.querySelector('#root')
