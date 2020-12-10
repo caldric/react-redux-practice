06642540ed75f661f4b9ceb3b02e892c42003d04
caldric <cvaldric@gmail.com>
2020-12-10 16:58:16 -0500

    Display video player on StreamShow

--- streams/client/src/components/streams/StreamShow.jsx
+++ streams/client/src/components/streams/StreamShow.jsx
@@ -1,24 +1,57 @@
-import React, { useEffect } from 'react';
+import React, { Component } from 'react';
 
 import { connect } from 'react-redux';
 import { fetchStream } from '../../actions';
+import flv from 'flv.js';
 
-const StreamShow = ({ fetchStream, match, stream }) => {
-  const streamID = match.params.id;
+class StreamShow extends Component {
+  constructor(props) {
+    super(props);
+    this.videoRef = React.createRef();
+  }
 
-  useEffect(() => {
-    fetchStream(streamID);
-  }, [fetchStream, streamID]);
+  streamID = this.props.match.params.id;
 
-  if (!stream) return <div>Loading...</div>;
+  componentDidMount() {
+    this.props.fetchStream(this.streamID);
+    this.buildPlayer();
+  }
 
-  return (
-    <div>
-      <h1>{stream.title}</h1>
-      <h5>{stream.description}</h5>
-    </div>
-  );
-};
+  componentDidUpdate() {
+    this.buildPlayer();
+  }
+
+  componentWillUnmount() {
+    this.player.destroy();
+  }
+
+  buildPlayer() {
+    if (this.player || !this.props.stream) {
+      return;
+    }
+
+    this.player = flv.createPlayer({
+      type: 'flv',
+      url: `http://localhost:8000/live/${this.streamID}.flv`,
+    });
+    this.player.attachMediaElement(this.videoRef.current);
+    this.player.load();
+  }
+
+  render() {
+    if (!this.props.stream) return <div>Loading...</div>;
+
+    const { title, description } = this.props.stream;
+
+    return (
+      <div>
+        <video ref={this.videoRef} style={{ width: '100%' }} controls={true} />
+        <h1>{title}</h1>
+        <h5>{description}</h5>
+      </div>
+    );
+  }
+}
 
 const mapStateToProps = (state, ownProps) => {
   const id = ownProps.match.params.id;
