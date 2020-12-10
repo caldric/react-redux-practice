3fdea96a5b41703ea2370acd136ca195d9417d6d
caldric <cvaldric@gmail.com>
2020-12-10 17:17:08 -0500

    Complete app mockup

--- translate/src/components/App.jsx
+++ translate/src/components/App.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+import UserCreate from './UserCreate';
+
 export default class App extends Component {
   state = { language: 'english' };
 
@@ -21,7 +23,7 @@ export default class App extends Component {
             onClick={() => this.onLanguageChange('dutch')}
           ></i>
         </div>
-        {this.state.language}
+        <UserCreate />
       </div>
     );
   }
---
+++ translate/src/components/UserCreate.jsx
@@ -0,0 +1,14 @@
+import Button from './Button';
+import Field from './Field';
+import React from 'react';
+
+const UserCreate = () => {
+  return (
+    <div className="ui form">
+      <Field />
+      <Button />
+    </div>
+  );
+};
+
+export default UserCreate;
