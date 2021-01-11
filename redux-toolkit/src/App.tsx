75097755bb0a2c0696bf445c29b603097a8f5565
caldric <cvaldric@gmail.com>
2021-01-11 15:23:44 -0500

    Create basic counter app

--- redux-toolkit/src/App.tsx
+++ redux-toolkit/src/App.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
 const App: React.FC = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <p>Counter: 0</p>
+      <button type="button">+</button>
+      <button type="button">-</button>
+    </div>
+  );
 };
 
 export default App;
