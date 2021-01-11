c6ca5454b2d6829bdaefc65379c956223323411b
caldric <cvaldric@gmail.com>
2021-01-11 15:58:18 -0500

    Attach state and action creators to app

--- redux-toolkit/src/App.tsx
+++ redux-toolkit/src/App.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { RootState } from './store';
+import { increment, decrement } from './counterSlice';
 
 const App: React.FC = () => {
+  const counter = useSelector((state: RootState) => state.counter);
+  const dispatch = useDispatch();
+
   return (
     <div>
-      <p>Counter: 0</p>
-      <button type="button">+</button>
-      <button type="button">-</button>
+      <p>Counter: {counter}</p>
+      <button type="button" onClick={() => dispatch(decrement())}>
+        Decrement
+      </button>
+      <button type="button" onClick={() => dispatch(increment())}>
+        Increment
+      </button>
     </div>
   );
 };
